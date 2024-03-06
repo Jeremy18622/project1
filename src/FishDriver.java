0024d9a9673c3332437c84c5221671a0e8fe8ab6
Jeremy18622 <162486947+Jeremy18622@users.noreply.github.com>
2024-03-06 11:44:24 -0600

    Update FishDriver.java

--- src/FishDriver.java
+++ src/FishDriver.java
@@ -2,11 +2,18 @@ public class FishDriver {
 
     public static void main(String[] args) {
 
-        Fish fish = new Fish("Fish", 12);
-        SaltWater saltyFish = new SaltWater("", 40, false);
-        FreshWater freshFish = new FreshWater("", 16, true);
-        Tarpon tarpon = new Tarpon("")
+        Fish fish = new Fish("John the Fish", 12);
+        SaltWater saltyFish = new SaltWater("Eric the saltwater fish", 40, false);
+        FreshWater freshFish = new FreshWater("Maddie the freshwater fish", 16, true);
+        Tarpon tarpon = new Tarpon("Tony the Tarpon", 60, true, "Pinfish");
+        Bass bass = new Bass("Billy the Bass", 21, false, "Bluegill");
+        Bluegill bluegill = new Bluegill("Ben the Bluegill", 6, false, "Minnows");
 
+        Fish[][] array = new Fish[[fish, saltyFish, freshFish], [tarpon, bass, bluegill]];
+
+        
+        
+        
     }
 
 }
